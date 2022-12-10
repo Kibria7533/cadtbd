20fe98055037860a421f9a16f470c51774676ed6
Golam_kibria <kibria7533@A>
2022-12-10 15:44:45 +0600

    before donate add

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -1,48 +1,53 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/career">Career<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/press">Press</a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="/admin">Login</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        </form>
-    </div>
-</nav>
-<div class="header container">
-    <div class="visible-xs visible-sm col-xs-12 col-sm-12 text-center sm-logo">
-        <a rel="home" href="/home">
-            <img src="images/cadtbd.png" width="200" alt="logo">
-        </a>
+<div class="bg-warning py-3">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/career">Career<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/press">Press</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/admin">Login</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+            </div>
+        </nav>
     </div>
 </div>
-<div class="navbar position-sticky" role="navigation">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+
+<!-- ============= COMPONENT ============== -->
+<nav id="navbar_top" class="navbar navbar-expand-lg navbar-light bg-light ">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-    </div>
-    <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-            <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/program">Program</a></li>
-            <li class= "hidden-xs hidden-sm">
-                <a rel="home" href="/home"><img class="logo" src="images/cadtbd.png" width="200" alt="logo"></a>
-            </li>
-            <li><a href="/event">Events</a></li>
-            <li><a href="/research">Research</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </div>
-</div>
+        <div class="collapse navbar-collapse d-flex justify-content-center" id="main_nav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="/home">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="/program">Program</a></li>
+                <li>
+                    <a rel="home" href="/home"><img class="logo" src="images/cadtbd.png" width="200" alt="logo"></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="/event">Events</a></li>
+                <li class="nav-item"><a class="nav-link" href="/research">Research</a></li>
+                <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+            </ul>
+        </div> <!-- navbar-collapse.// -->
+    </div> <!-- container-fluid.// -->
+</nav>
+
+<style>
+    .nav-link{
+        color: rgb(36, 148, 160);
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 15px;
+    }
+</style>
--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -17,6 +17,28 @@
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        document.addEventListener("DOMContentLoaded", function(){
+
+            window.addEventListener('scroll', function() {
+
+                if (window.scrollY > 50) {
+                    document.getElementById('navbar_top').classList.add('fixed-top');
+                    // add padding top to show content behind navbar
+                    navbar_height = document.querySelector('.navbar').offsetHeight;
+                    document.body.style.paddingTop = navbar_height + 'px';
+                } else {
+                    document.getElementById('navbar_top').classList.remove('fixed-top');
+                    // remove padding top from body
+                    document.body.style.paddingTop = '0';
+                }
+            });
+        });
+        // DOMContentLoaded  end
+    </script>
     <style>
         .vertical-menu {
             width: 200px;
